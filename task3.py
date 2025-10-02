76c9472c3d407438211ee5f85d91bd0dafce0311
Владислав Кондратенко <148567173+vampyrials@users.noreply.github.com>
2025-10-03 00:07:05 +0300

    task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,8 @@
-# Створюємо список з N перших чисел, кратних 22
 N = int(input("Введіть N: "))
 numbers = [22 * i for i in range(1, N + 1)]
 
 print("Початковий список:", numbers)
 
-# Беремо останні 3 елементи
-last_three = numbers[-3:] if len(numbers) >= 3 else numbers
+ёlast_three = numbers[-3:] if len(numbers) >= 3 else numbers
 
 print("Останні 3 елементи:", last_three)
